bbd4546fbc9dfa439800f67cf02954366e6af889
Robin Moussu <robin.moussu+git@gmail.com>
2015-12-06 13:16:29 +0100

    update wc.sh script to ignore story.tex in count

--- story/wc.sh
+++ story/wc.sh
@@ -1,1 +1,1 @@
-grep -v section -h *.tex | grep -v '%' | sed 's/\\\w\+\(\[.*\]\)*{.*}//g' | wc -w
+grep -v section -h $(ls *.tex | grep -v story.tex) | grep -v '%' | sed 's/\\\w\+\(\[.*\]\)*{.*}//g' | wc -w
